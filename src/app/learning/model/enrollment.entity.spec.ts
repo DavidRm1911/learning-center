9fbedcaf2c6aa914caa3d85206114e369a79f958
DavidRm1911 <134657480+DavidRm1911@users.noreply.github.com>
2024-09-17 19:35:13 -0500

    feat(learning): add domain model entities for the learning bounded context

---
+++ src/app/learning/model/enrollment.entity.spec.ts
@@ -0,0 +1,8 @@
+import { Enrollment } from './enrollment.entity';
+
+describe('Enrollment', () => {
+  it('should create an instance', () => {
+    // @ts-ignore
+    expect(new Enrollment()).toBeTruthy();
+  });
+});
