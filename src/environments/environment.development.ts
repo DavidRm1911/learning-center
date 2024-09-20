d4080a69b14e003b990dfc944a757fe714806bba
DavidRm1911 <134657480+DavidRm1911@users.noreply.github.com>
2024-09-19 19:50:02 -0500

    chore: configure routes for SPA

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  serverBasePath: 'https://my-json-server.typicode.com/DavidRm1911/learning-centeer/courses'
+  serverBasePath: 'https://my-json-server.typicode.com/DavidRm1911/learning-centeer'
 
   //serverBasePath: 'httpL//localhost:8080/api/v1'
 };
